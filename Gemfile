78e5c2870779bff2a243edfd45d834af37f60058
John Brady <bradyjoh@grinnell.edu>
2014-10-09 11:46:25 -0500

    Updates for Heroku (A.8)

--- Gemfile
+++ Gemfile
@@ -5,7 +5,13 @@ gem 'rails', '3.2.18'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+group :development, :test do
+  gem 'sqlite3' #develop locally with the sqlite database
+end
+
+group :production do
+  gem 'pg' #Heroku uses postgres
+end
 
 
 # Gems used only for assets and not required
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -64,5 +66,7 @@ module Foodpantry
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile=false
   end
 end
