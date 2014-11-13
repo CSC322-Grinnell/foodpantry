95fd32363fa1e4d367083b5be9090f7d97084e93
leamars <maroltso@grinnell.edu>
2014-11-13 14:22:58 -0600

    Cucumber scenarios were broken due to haml, but they are fixed now. Rejoice.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,57 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-foods = [{:name => "Canned Food"},
+foods = [{:name => "100% Juice Concentrate/Boxes"},
+			{:name => "Canned Beans"},
+			{:name => "Boxed Potatoes"},
+			{:name => "Fresh Produce"},
+			{:name => "Canned Beans"},
+			{:name => "Boxed Potatoes"},
+			{:name => "Frozen Foods"},
+			{:name => "Canned Fruits"},
+			{:name => "Granola Bars"},
+			{:name => "Jelly"},
+			{:name => "Mac 'n Cheese"},
+			{:name => "Canned Meat"},
+			{:name => "Miscellaneous Food Items"},
+			{:name => "Oatmeal"},
+			{:name => "Pancake Mix"},
+			{:name => "Pasta & Pasta Sauce"},
+			{:name => "Peanut Butter"},
+			{:name => "Ramen"},
+			{:name => "Refrigerated Food"},
+			{:name => "Rice"},
+			{:name => "Skillet Dinners"},
+			{:name => "Canned Soup"},
+			{:name => "Syrup"},
+			{:name => "Canned Tuna"},
+			{:name => "Canned Vegetables"},
+			{:name => "Baby Food"},
+			{:name => "Baby Wipes"},
+			{:name => "Baby Wash"},
+			{:name => "Children's Book"},
+			{:name => "Deodorant"},
 			{:name => "Diapers"},
-			{:name => "Apples"},
-			{:name => "Butter"},
+			{:name => "Dishwashing Liquid"},
+			{:name => "Floss"},
+			{:name => "Infant Formula"},
+			{:name => "Laundry Detergent"},
+			{:name => "Paper Towels"},
+			{:name => "Razers & Shaving Cream"},
+			{:name => "Shampoo & Conditioner"},
+			{:name => "Soap"},
+			{:name => "Tampons"},
+			{:name => "Tissues"},
+			{:name => "Toilet Paper"},
+			{:name => "Toothbrushes"},
 		]
 foods.each do |fud|
 	FoodItem.create!(fud)
+end
+
+contacts = [{:hours => "Whenever, where ever", :email => "mica@shakira.org", :street_location => "1001 S. 18th Ave.", :state_location => "BellyRoll, IA 50112", :phone_number => "911", :holiday_hours => "NEVER EVER"},
+]
+
+contacts.each do |con|
+	ContactInfo.create!(con)
 end
