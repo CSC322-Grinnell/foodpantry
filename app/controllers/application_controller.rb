12303ac2517b47b6ee77700e822efad3f71c9c05
Patrick Slough <sloughpa@grinnell.edu>
2015-04-14 19:41:34 -0500

    fixed model-call-in-view hack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :get_footer_info
+  def get_footer_info
+  	@footer_info = ContactInfo.all
+  end
 end
