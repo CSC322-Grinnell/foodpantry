e320a8e42e5cc890fb6301b06cdb1ca176d357a1
Patrick Slough <sloughpa@grinnell.edu>
2015-04-21 20:17:11 -0500

    started drop down menu stuff

--- app/admin/food_items.rb
+++ app/admin/food_items.rb
@@ -2,4 +2,17 @@ ActiveAdmin.register FoodItem do
     action_item only: :index do
         link_to 'Reset Item Priority', reset_food_items_path
     end
+
+	form partial: 'form'
+
+	controller do
+		def show
+			@food_item = FoodItem.find(params[:id])
+			if @food_item.save
+				redirect_to admin_model_path(@food_item)
+			else
+				render :edit
+			end
+		end
+	end
 end
