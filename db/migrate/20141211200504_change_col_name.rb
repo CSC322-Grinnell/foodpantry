a0f44ac6b788f92165ef0120bc2a0bae1b8b89bd
John Brady <bradyjoh@grinnell.edu>
2015-05-05 21:14:38 -0500

    removed unneccesary migration

--- db/migrate/20141211200504_change_col_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColName < ActiveRecord::Migration
-  def up
-  	rename_column :food_items, :needPriority, :needpriority
-  end
-
-  def down
-  	rename_column :food_items, :needpriority, :needPriority
-  end
-end
